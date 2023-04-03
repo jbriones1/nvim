464311b7d3411f587deb27590a54b67aee50ebe9
jbriones <jbriones@fortinet.com>
2023-04-03 10:34:38 -0700

    Got autocompletion working

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,9 +1,22 @@
-local lsp = require('lsp-zero').preset({})
+local lsp = require('lsp-zero')
+
+lsp.preset('recommended')
+
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+local cmp = require('cmp')
+-- local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+    ['<C-Space>'] = cmp.mapping.complete()
+})
+
+lsp.setup_nvim_cmp({
+    mapping = cmp_mappings
+})
 
 lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({buffer = bufnr})
 end)
-
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
