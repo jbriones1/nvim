417303a8ea0e5a9ac472cf12ba577651ba01f4b0
jbriones <jbriones@fortinet.com>
2023-04-03 20:00:23 -0700

    Changed nvim-tree to not open when finding a file

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -2,16 +2,13 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 require('nvim-tree').setup({
-	view = {
-		side = 'left',
-		mappings = {
-			list = {
-				{ key = 'u', action ='dir_up' }
-			},
-		},
-	},
-	update_focused_file = {
-		enable = true
-	}
+    view = {
+        side = 'left',
+        mappings = {
+            list = {
+                { key = 'u', action ='dir_up' }
+            },
+        },
+    }
 })
 map('n', '<C-b>', ':NvimTreeToggle<CR>')
