44de1bcd498e9cce61671ae70f1e621a0e7166b4
jbriones <jbriones@fortinet.com>
2023-04-11 12:37:31 -0700

    Changed telescope path display

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,18 @@
 require('telescope').setup {
     defaults = {
-        path_display={'shorten'}
+        sorting_strategy = 'ascending',
+        layout_config = {
+            horizontal = {
+                prompt_position = 'top',
+            }
+        },
+        path_display = function(opts, path)
+            local tail = require('telescope.utils').path_tail(path)
+            return string.format('%s (%s)', tail, path)
+        end
     }
 }
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
