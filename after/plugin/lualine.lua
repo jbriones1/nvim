44cceedb89e7f406eb729f427a261dc301b7248b
jbriones <jbriones@fortinet.com>
2023-04-11 12:46:04 -0700

    Added file path to lua line

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,5 +1,17 @@
 require('lualine').setup({
     options = {
         theme = 'vscode'
+    },
+    sections = {
+        lualine_c = {
+            {
+                'filename',
+                path = 3,
+                symbols = {
+                    modified = '[Modified]',
+                    readonly = '[Read only]'
+                }
+            }
+        }
     }
 })
