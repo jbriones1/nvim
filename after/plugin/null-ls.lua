58956fa4448978168b221ed7cf96d50efdd87872
jbriones <jbriones@fortinet.com>
2023-04-18 14:31:51 -0700

    Added prettierd support to javascript and html. - Disabled format on save to javascript and html

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -1,9 +1,9 @@
-local null_ls = require("null-ls")
+local null_ls = require('null-ls')
 local mason_null_ls = require('mason-null-ls')
 
-local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
-local event = "BufWritePre" -- or "BufWritePost"
-local async = event == "BufWritePost"
+local group = vim.api.nvim_create_augroup('lsp_format_on_save', { clear = false })
+local event = 'BufWritePre' -- or 'BufWritePost'
+local async = event == 'BufWritePost'
 
 mason_null_ls.setup({
     automatic_setup = true
@@ -13,41 +13,44 @@ null_ls.setup({
     sources = {
         null_ls.builtins.formatting.prettierd.with({
             filetypes = {
-                "css",
-                -- "html",
-                "json",
-                "less",
-                "markdown",
-                "scss",
-                "typescript",
-                "typescriptreact",
-                "yaml"
+                'css',
+                'html',
+                'json',
+                'less',
+                'markdown',
+                'scss',
+                'typescript',
+                'typescriptreact',
+                'javascript',
+                'yaml'
             }
         }),
         null_ls.builtins.diagnostics.eslint_d
     },
     on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.keymap.set("n", "<leader>f", function()
+        if client.supports_method('textDocument/formatting') then
+            vim.keymap.set('n', '<leader>f', function()
                 vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf(), timeout_ms = 3000 })
-            end, { buffer = bufnr, desc = "[lsp] format" })
+            end, { buffer = bufnr, desc = '[lsp] format' })
 
-            -- format on save
-            vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
-            vim.api.nvim_create_autocmd(event, {
-                buffer = bufnr,
-                group = group,
-                callback = function()
-                    vim.lsp.buf.format({ bufnr = bufnr, async = async })
-                end,
-                desc = "[lsp] format on save",
-            })
+            -- format on save, except for html
+            if vim.bo.filetype ~= 'html' or vim.bo.filetype ~= 'javascript' then
+                vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+                vim.api.nvim_create_autocmd(event, {
+                    buffer = bufnr,
+                    group = group,
+                    callback = function()
+                        vim.lsp.buf.format({ bufnr = bufnr, async = async })
+                    end,
+                    desc = '[lsp] format on save',
+                })
+            end
         end
 
-        if client.supports_method("textDocument/rangeFormatting") then
-            vim.keymap.set("x", "<Leader>f", function()
+        if client.supports_method('textDocument/rangeFormatting') then
+            vim.keymap.set('x', '<Leader>f', function()
                 vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-            end, { buffer = bufnr, desc = "[lsp] format" })
+            end, { buffer = bufnr, desc = '[lsp] format' })
         end
     end,
 })
