bdd56b1615c2e6d216f4a12e9c2cb7adc04c4718
jbriones <jbriones@fortinet.com>
2023-04-21 23:38:55 -0700

    Added packer bootstrapping

--- lua/main/packer.lua
+++ lua/main/packer.lua
@@ -1,4 +1,17 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
@@ -96,4 +109,10 @@ return require('packer').startup(function(use)
 
     -- {{ Session management }}
     use 'rmagatti/auto-session'
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
