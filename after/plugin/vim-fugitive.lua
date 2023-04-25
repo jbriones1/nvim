a80572bf70e6e6cdc732601e8627c5dd1dfbe63d
jbriones <jbriones@fortinet.com>
2023-04-25 11:55:45 -0700

    Added some easier merge conflict resolution

--- after/plugin/vim-fugitive.lua
+++ after/plugin/vim-fugitive.lua
@@ -1,2 +1,4 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = 'Git status' })
 vim.keymap.set("n", "<leader>gf", ':0Gclog<CR>', { desc = 'Git file history' })
+vim.keymap.set("n", "<leader>gD", ':Gvdiffsplit!<CR>', { desc = 'Merge tool' })
+vim.keymap.set("n", "<leader>gW", ':Gwrite<CR>', { desc = 'Commit afte merging' })
