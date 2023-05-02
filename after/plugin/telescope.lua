55e667612a30bfd4a7a8e745593f0439eb97e9b9
jbriones <jbriones@fortinet.com>
2023-05-01 20:46:10 -0700

    Added some commands to telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,6 @@
 local telescope = require('telescope')
+local builtin = require('telescope.builtin')
+local actions = require('telescope.actions')
 
 telescope.setup {
     defaults = {
@@ -11,7 +13,15 @@ telescope.setup {
         path_display = function(opts, path)
             local tail = require('telescope.utils').path_tail(path)
             return string.format('%s (%s)', tail, path)
-        end
+        end,
+        mappings = {
+            i = {
+                ['<Esc>'] = actions.close,
+                -- ['<C-o>'] = actions.select_default + builtin.resume,
+                ['<C-j>'] = actions.move_selection_next,
+                ['<C-k>'] = actions.move_selection_previous,
+            }
+        }
     },
     extension = {
         ['ui-select'] = {
@@ -22,10 +32,10 @@ telescope.setup {
 
 telescope.load_extension('ui-select')
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Find files (non-git)' })
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
+vim.keymap.set('n', '<C-g>', builtin.resume, { desc = 'Open previous telescope window' })
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input('Grep > ') })
 end)
@@ -38,4 +48,4 @@ vim.keymap.set('n', '<leader>o', builtin.lsp_document_symbols, {})
 vim.keymap.set('n', '<leader>b', function()
     builtin.buffers({ only_cwd = true })
 end)
-vim.keymap.set('n', '<leader>pvf', builtin.oldfiles, {})
+vim.keymap.set('n', '<leader>pvf', builtin.oldfiles, { desc = 'Previous files' })
